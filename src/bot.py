43b89848a6a46f4695586a14eb13a0822cb4da2b
Pooya Shirazi <hydrocodeir@gmail.com>
2025-03-11 18:48:28 +0000

    ...

--- src/bot.py
+++ src/bot.py
@@ -6,11 +6,14 @@ from handlers import handlers
 
 
 if __name__ == "__main__":
+    print("Bot Started :)")
     logger.info("Bot Started!")
     try:
         bot.polling(none_stop=True)
     except Exception as e:
+        print("Bot Crashed :(")
         logger.error(f"Bot Crashed: {e}")
         time.sleep(2)
+        print("Bot Crashed :|")
         logger.info("Bot Restarted!")
         bot.polling(none_stop=True)
--- src/config.py
+++ src/config.py
@@ -7,8 +7,8 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path=dotenv_path)
 
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_CHAT_ID = os.getenv('ADMIN_CHAT_ID')
+BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
+ADMIN_CHAT_ID = int(os.getenv('ADMIN_CHAT_ID'))
 
 
 bot = TeleBot(token=BOT_TOKEN)
